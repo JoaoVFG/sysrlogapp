403dfd2a716271db26ed4958d271538758eff8bc
Joao Vitor <=>
2018-08-25 17:29:13 -0300

    Menu desabilitado na tela de Login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { loginDTO } from '../../models/login.dto';
 import { loginService } from '../../Service/login.service';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -17,21 +18,27 @@ export class HomePage {
   
   token : string;
   constructor(public navCtrl: NavController,
-              public loginService: loginService) {
+              public loginService: loginService,
+              public menu: MenuController) {
 
   }
 
   login(){
-    
     this.loginService.authenticate(this.logindto)
       .subscribe(response => {
         this.token = response.body.toString();
         this.loginService.sucessfullAuthentication(this.token);      
       },
       error => {
-        
       })
-  
+  }
+
+  ionViewWillEnter(){
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewDidLeave(){
+    this.menu.swipeEnable(true);
   }
 
 }
