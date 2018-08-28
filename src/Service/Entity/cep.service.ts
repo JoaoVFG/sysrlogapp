d3139302ccbc925416c2cc24062ab41a8f5f18d1
Joao Vitor <=>
2018-08-27 21:55:56 -0300

    021 - Criado Cep Service e cep DTO

---
+++ src/Service/Entity/cep.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { cep } from "../../models/cep.dto";
+import { API_CONFIG } from "../../Config/api.config";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class CepService{
+    
+    constructor(public http : HttpClient){
+        
+    }
+
+    findByCep(cep : String) : Observable<cep>{
+        return this.http.get<cep>(`${API_CONFIG.baseUrl}/cep/buscacep/` + cep);
+    }
+
+    findAllCep() : Observable<cep[]>{
+        return this.http.get<cep[]>(`${API_CONFIG.baseUrl}/cep/buscacep/`);
+    }
+
+}
---
+++ src/models/cep.dto.ts
@@ -0,0 +1,7 @@
+export interface cep{
+    cep : string;
+	nomeRua : string;
+	nomeBairro : string;
+	nomeCidade : string;
+	nomeEstado : string;
+}
