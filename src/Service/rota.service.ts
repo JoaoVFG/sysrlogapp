ce4f5ff7c48a810eb2a6ace2597d9d6d041fc8e6
Joao Vitor <=>
2018-09-18 16:13:16 -0300

    035 - Implementado métodos de consulta em rota service

--- src/Service/rota.service.ts
+++ src/Service/rota.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { listaEnderecoEntregaDTO } from "../models/rota/listaenderecoentrega.dto";
 import { API_CONFIG } from "../Config/api.config";
+import { rotaResponse } from "../models/rota/rotaresponse.dto";
+import { Observable } from "rxjs";
+import { storageService } from "./storage.service";
 
 @Injectable()
 export class RotaService{
 
-    constructor(public http : HttpClient){
+    constructor(public http : HttpClient,
+                public storage : storageService){
         
     }
 
@@ -18,6 +22,18 @@ export class RotaService{
                 observe: 'response',
                 responseType: 'text'
             }
+        )
+    }
+
+    findById(idRota : number) : Observable<rotaResponse>{
+        return this.http.get<rotaResponse>(
+            `${API_CONFIG.baseUrl}/rota/busca/id/` + idRota
+        )
+    }
+
+    findByUser() : Observable<rotaResponse[]>{
+        return this.http.get<rotaResponse[]>(
+            `${API_CONFIG.baseUrl}/rota/busca/` + this.storage.retrieveIdUser()
         )
     }
 
