4b4614b717b4b4f1dbf8f321b720524b5dfc79ee
JoaoVFG <joao.vfg@hotmail.com.br>
2018-09-20 01:14:14 -0300

    039 - Criado Formulário para Atualização de Pessoa

--- src/pages/pessoa/pessoa.ts
+++ src/pages/pessoa/pessoa.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { PessoaService } from '../../Service/Entity/pessoa.service';
 import { Pessoa } from '../../models/pessoa.dto';
 import { LoadingService } from '../../Service/Components/loading.service';
 import { User } from '../../models/user.dto';
 import { UserService } from '../../Service/Entity/user.service';
 import { storageService } from '../../Service/storage.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the PessoaPage page.
@@ -29,13 +30,28 @@ export class PessoaPage {
     senha: '',
     pessoa: undefined,
   }
-  
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              public pessoaService : PessoaService,
-              public loadingService: LoadingService,
-              public userService: UserService,
-              public storage: storageService,) {
+  modo: string = 'Consulta';
+  formGroup: FormGroup;
+
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public pessoaService: PessoaService,
+    public loadingService: LoadingService,
+    public userService: UserService,
+    public storage: storageService,
+    public alertController: AlertController,
+    public formBuilder: FormBuilder) {
+
+    this.formGroup = this.formBuilder.group({
+      id: [''],
+      nome: [''],
+      razaoSocial: [''],
+      cpf: [''],
+      cnpj: [''],
+      dataNascimento: ['',],
+      sexo: [''],
+    });
   }
 
   ionViewDidLoad() {
@@ -44,6 +60,8 @@ export class PessoaPage {
     this.userService.findById(this.storage.retrieveIdUser())
       .subscribe(responseUser => {
         this.pessoa = responseUser.pessoa;
+        console.log(this.pessoa);
+        
       },
         error => {
           console.log(error);
@@ -53,8 +71,51 @@ export class PessoaPage {
     loading.dismiss();
   }
 
-  altera(){
-    console.log('Altera');
-    
+  alteraPessoa() {
+    let updatePessoa: Pessoa = {
+      id: this.pessoa.id,
+      nome: this.formGroup.value.nome,
+      cpf: this.formGroup.value.cpf,
+      dataNascimento: this.formGroup.value.dataNascimento,
+      sexo: this.formGroup.value.sexo,
+      razaoSocial: this.formGroup.value.razaoSocial,
+      cnpj: this.formGroup.value.cnpj,
+      tipo: undefined
+    }
+    console.log(updatePessoa)
+    this.pessoaService.updatePessoa(updatePessoa)
+      .subscribe((response) =>{
+        console.log(response);
+        
+      },error =>{
+        console.log(error);
+        
+      });
+    this.showUpdateOk();
+
+
+
+  }
+
+  alteraModo(newModo: string) {
+    this.modo = newModo;
+  }
+
+  showUpdateOk() {
+    let alert = this.alertController.create({
+      title: 'Sucesso!',
+      message: 'Cadastro Alterado com sucesso!',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'ok',
+          handler: () => {
+            this.navCtrl.setRoot('PessoaPage');
+            this.alteraModo('Consulta');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
